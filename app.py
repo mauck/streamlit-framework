1a81fe29c3275afe0d62173f28a465c8a2f02c5f
china <mauck@math.utah.edu>
2022-01-08 21:31:42 -0600

    Using widgets

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-import requests
-
 # This is from the streamlit get started tutorial
 # https://docs.streamlit.io/library/get-started/main-concepts
 
 import streamlit as st
-import pandas as pd
-import numpy as np
-
-map_data = pd.DataFrame(
-	np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-	columns=['lat', 'lon'])
-
-st.map(map_data)
+x = st.slider('x')
+st.write(x, 'squared is', x*x)
