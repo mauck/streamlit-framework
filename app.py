31b06cdecb207b1b7cf2046304b0f5c90d1c5b3d
china <mauck@math.utah.edu>
2022-01-09 17:11:00 -0600

    Adding selectbox for stock

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-# Sample Python Integration Code for Alpha Vantage
-# From:
-# https://medium.com/@patrick.collins_58673/stock-api-landscape-5c6e054ee631
-
+import os
+import streamlit as st
 import requests
-import json
+import simplejson as json
+import pandas as pd
+import csv
 
-key = 'XXX'
-ticker = 'AAPL'
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
+# Get Alpha Vantage API key
+key = os.environ.get('KEY')
 
+# Get list of active stocks
+stocks_url = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={}'.format(key)
+stocks = pd.read_csv(stocks_url)
+# st.dataframe(stocks)
+
+# Choose stock ticker
+# ticker = st.selectbox('Choose stock ticker:', ('AAPL', 'GOOG'))
+ticker = st.selectbox('Choose a stock ticker:', stocks['symbol'])
+
+# Make API request
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}'.format(ticker, key)
 response = requests.get(url)
-print(response.json())
+
+# Extract data
+data = response.json()
+df=pd.DataFrame.from_dict(data['Time Series (Daily)'], orient="index")
+
+# Display resulting pandas data frame
+# st.dataframe(df)
+
+# Plot closing values
+st.line_chart(df['4. close'][0:30])
