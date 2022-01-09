b6b89c7c02f22509a5bfc2b0c4c039705010717a
china <mauck@math.utah.edu>
2022-01-08 21:39:56 -0600

    More widgets

--- app.py
+++ app.py
@@ -2,5 +2,6 @@
 # https://docs.streamlit.io/library/get-started/main-concepts
 
 import streamlit as st
-x = st.slider('x')
-st.write(x, 'squared is', x*x)
+st.text_input("Your name", key="name")
+
+st.session_state.name
