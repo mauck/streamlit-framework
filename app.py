a71b037d3530acf852b76b8bfd818f21a483aa1b
china <mauck@math.utah.edu>
2022-01-08 19:32:51 -0600

    Using pandas styler

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+dataframe = pd.DataFrame(
+	np.random.randn(10, 20),
+	columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
