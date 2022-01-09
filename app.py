b282527578cd2a1bda7fec337bb3158d860a506e
china <mauck@math.utah.edu>
2022-01-08 18:49:46 -0600

    Update using numpy to generate a random sample

--- app.py
+++ app.py
@@ -6,8 +6,5 @@ import requests
 import streamlit as st
 import pandas as pd
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-	'first column': [1, 2, 3, 4],
-	'second column': [10, 20, 30, 40]
-	}))
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
